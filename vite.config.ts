bed1d371e83187489e0d30ec9a89398cd10beec9
AmirZalaghi <zalaghi.dev@gmail.com>
2026-01-26 20:26:48 +0330

    add: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE || "/",
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
